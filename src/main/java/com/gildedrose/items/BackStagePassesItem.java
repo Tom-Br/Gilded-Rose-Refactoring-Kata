94d92add9d400a71a3228cb9fcbfdd3bd5212ea2
Tom Bruyninx <tom.bruyninx@gmail.com>
2019-11-14 02:19:36 +0100

    implement updateItem for BackStagePasses

--- src/main/java/com/gildedrose/items/BackStagePassesItem.java
+++ src/main/java/com/gildedrose/items/BackStagePassesItem.java
@@ -20,7 +20,31 @@ public class BackStagePassesItem extends Item implements ShopItem, QualityCheck 
 
     @Override
     public void updateItem() {
+        updateSellin();
+        if(sellIn <= 0){
+            quality = QUALITY_LEVEL.getMin();
+            return;
+        }
+        improve();
+        if(quality > QUALITY_LEVEL.getMax()){
+            quality = QUALITY_LEVEL.getMax();
+        }
+    }
 
+    private void improve() {
+        if(sellIn > 10){
+            quality++;
+        }
+        else if(sellIn > 5){
+            quality += 2;
+        }
+        else if(sellIn > 0){
+            quality += 3;
+        }
+    }
+
+    private void updateSellin(){
+        sellIn--;
     }
 
     @Override
